4c0b82e77016e25e4a8866bfd34c45a86d51823b
Styks <bloodstyks@gmail.com>
2019-07-04 00:40:13 +0300

    Edit MainForm, Add GearForm, Add Images

---
+++ BDO_Builder/GearForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BDO_Builder
+{
+    public partial class GearForm : Form
+    {
+
+        public string sclass;
+
+        public GearForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GearForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing) Application.Exit();
+        }
+
+        private void GearForm_Load(object sender, EventArgs e)
+        {
+            Sclass_lbl.Text = sclass;
+        }
+    }
+}
